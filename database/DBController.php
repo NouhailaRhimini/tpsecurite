c64932af2e3f80f7c4c31d06b4e50844ab2bc560
NouhailaRhimini <65727538+NouhailaRhimini@users.noreply.github.com>
2021-03-02 17:00:44 +0000

    Update DBController.php

--- database/DBController.php
+++ database/DBController.php
@@ -4,17 +4,11 @@
 class DBController
 {
     // Database Connection Properties
-    //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["eu-cdbr-west-03.cleardb.net"];
-$cleardb_username = $cleardb_url["b5d53262c8e8f5"];
-$cleardb_password = $cleardb_url["7df5b121"];
-$cleardb_db = substr($cleardb_url["heroku_a1c43d1ef89e06b"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+     // Database Connection Properties
+    protected $host = 'eu-cdbr-west-03.cleardb.net';
+    protected $user = 'b5d53262c8e8f5';
+    protected $password = '7df5b121';
+    protected $database = "heroku_a1c43d1ef89e06b";
 
     // connection property
     public $con = null;
