30951a9b6b8daeef1e3300bf70142111efc565b9
NouhailaRhimini <65727538+NouhailaRhimini@users.noreply.github.com>
2021-03-02 16:14:00 +0000

    Update DBController.php

--- database/DBController.php
+++ database/DBController.php
@@ -4,10 +4,17 @@
 class DBController
 {
     // Database Connection Properties
-    protected $host = 'eu-cdbr-west-03.cleardb.net';
-    protected $user = 'b5d53262c8e8f5';
-    protected $password = '7df5b121';
-    protected $database = "shopee";
+    //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 
     // connection property
     public $con = null;
